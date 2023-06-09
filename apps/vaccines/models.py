be13a1d0c44024e2cc5c1dd091daf947538766d7
DanielaO85 <daniela.da.silva@gmail.com>
2023-06-09 20:20:33 -0300

    v1.10 Inclusões de métodos para Models vaccines.

--- apps/vaccines/models.py
+++ apps/vaccines/models.py
@@ -18,8 +18,49 @@ class Vacina:
         self._fornecedor = fornecedor  
         
      def adicionar_vacina(vacinas, id, nome_vacina, tipo_vacina,doses_necessarias, forma_armazenamento, temperatura, intervalo_entre_doses, numero_lote, fabricada_em, validade, fornecedor):
-      
         nova_vacina = Vacina(id, nome_vacina, tipo_vacina, doses_necessarias, forma_armazenamento, temperatura, intervalo_entre_doses, numero_lote, fabricada_em, validade, fornecedor)
         vacinas.append(nova_vacina)
         print("Vacina adicionada com sucesso!")
 
+     @staticmethod
+     def listar_vacinas(vacinas):
+          for vacina in vacinas:
+            print(f"ID: {vacina._id}")
+            print(f"Nome: {vacina._nome_vacina}")
+            print(f"Tipo: {vacina._tipo_vacina}")
+            print(f"Doses Necessárias: {vacina._doses_necessarias}")
+            print(f"Forma de Armazenamento: {vacina._forma_armazenamento}")
+            print(f"Temperatura: {vacina._temperatura}")
+            print(f"Intervalo entre Doses: {vacina._intervalo_entre_doses}")
+            print(f"Número do Lote: {vacina._numero_lote}")
+            print(f"Fabricada em: {vacina._fabricada_em}")
+            print(f"Validade: {vacina._validade}")
+            print(f"Fornecedor: {vacina._fornecedor}")
+            print()
+            
+     def checar_validade(vacinas):
+        data_atual = date.today()
+        vacinas_vencidas = []
+
+        for vacina in vacinas:
+          data_validade = date.fromisoformat(vacina._validade)
+          if data_validade < data_atual:
+              vacinas_vencidas.append(vacina)
+
+        if len(vacinas_vencidas) == 0:
+          print("Não há vacinas vencidas.")
+        else:
+          print("Vacinas vencidas:")
+          for vacina in vacinas_vencidas:
+            print(f"ID: {vacina._id}")
+            print(f"Nome: {vacina._nome_vacina}")
+            print(f"Validade: {vacina._validade}")
+            print()
+
+class Meta:
+        verbose_name = 'Vacina'
+        verbose_name_plural = 'Vacinas'
+        ordering =['id']
+
+def __str__(self):
+        return self.name

1088e4f8a0b58e5e1486a6212658377210ce0936
DanielaO85 <daniela.da.silva@gmail.com>
2023-06-09 20:11:52 -0300

    v1.8 Inclusão de models do app vaccines.

--- apps/vaccines/models.py
+++ apps/vaccines/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from vaccines.models import Vacina
+from datetime import date
 
-# Create your models here.
+class Vacina:
+    
+     def __init__(self, id, nome_vacina, tipo_vacina, doses_necessarias, forma_armazenamento, temperatura, intervalo_entre_doses, numero_lote, fabricada_em, validade, fornecedor):
+        self._id = id          
+        self._nome_vacina = nome_vacina
+        self._tipo_vacina = tipo_vacina
+        self._doses_necessarias = doses_necessarias
+        self._forma_armazenamento = forma_armazenamento
+        self._temperatura = temperatura
+        self._intervalo_entre_doses = intervalo_entre_doses
+        self._numero_lote = numero_lote
+        self._fabricada_em = fabricada_em
+        self._validade = validade
+        self._fornecedor = fornecedor  
+

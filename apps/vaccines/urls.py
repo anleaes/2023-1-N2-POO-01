bfe5972b7dd518f40b0889fbf4452cf5001dbb19
DanielaO85 <daniela.da.silva@gmail.com>
2023-06-09 20:52:45 -0300

    v1.17 urls concluido

--- apps/vaccines/urls.py
+++ apps/vaccines/urls.py
@@ -4,4 +4,9 @@ from rest_framework import routers
 
 app_name = 'vaccines'
 
+router = routers.DefaultRouter()
+router.register('Vacina', views.VacinaViewSet, basename='Vacina')
 
+urlpatterns = [
+    path('', include(router.urls) )
+]

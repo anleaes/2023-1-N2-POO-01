a18e1676a85197e532cd9873bf90b8f7b9cbec42
DanielaO85 <daniela.da.silva@gmail.com>
2023-06-09 21:40:26 -0300

    v1.26 Conclusão arquivo views

--- apps/vaccineSupplier/views.py
+++ apps/vaccineSupplier/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import FornecedorVacina
 
+from rest_framework import viewsets
+from .serializer import FornecedorVacinaSerializer
+
+class FornecedorVacinaViewSet(viewsets.ModelViewSet):
+    queryset = FornecedorVacina.objects.all()
+    serializer_class = FornecedorVacinaSerializer  

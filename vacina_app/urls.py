9eeb90d6bdc8210ff8761e3a33a00179021c8924
eduardalutz <eduarda.lutz.silva@gmail.com>
2023-06-10 17:02:01 -0300

    update

--- vacina_app/urls.py
+++ vacina_app/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('funcionarios/', include('employee.urls', namespace='employee')),
-    path('localvacina/', include('vacinationLocal.urls', namespace='vacinationLocal')),
 ]

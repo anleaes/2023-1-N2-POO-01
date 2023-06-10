db997fe6c6c3a298d3c389812560402078485d3f
eduardalutz <eduarda.lutz.silva@gmail.com>
2023-06-10 13:59:40 -0300

    update models

--- apps/vacinationLocal/models.py
+++ apps/vacinationLocal/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class LocalVacina:
+    def __init__(self, nome, endereco):
+        self.nome = nome
+        self.endereco = endereco
+
+    # Getters e Setters
+
+    def get_nome(self):
+        return self.nome
+
+    def set_nome(self, nome):
+        self.nome = nome
+
+    def get_endereco(self):
+        return self.endereco
+
+    def set_endereco(self, endereco):
+        self.endereco = endereco

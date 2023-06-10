b956a50c24ddfbc1212f31cbb89d1917df5bc6ec
Mirian <130416417+mirianns@users.noreply.github.com>
2023-06-10 18:00:11 -0300

    alteração

--- apps/pacientVacines/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import pacientVacines
-
-class PacienteSerializer(serializers.ModelSerializer):
-    local = vacinationLocalSerializer()
-
-    class Meta:
-        model = pacientVacines
-        fields = ('id', 'dose', 'funcionario', 'aplicada_em', 'paciente')

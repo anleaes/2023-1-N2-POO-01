dd515cca90ff7ffd52420bf9db7166fc02a2fa89
Rafael Machado <rafaeldmachado2001@gmail.com>
2023-06-11 00:03:16 -0300

    Adicionada a configuração da classe 'pacientVacines' no arquivo 'settings'

--- vacina_app/settings.py
+++ vacina_app/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'employee.apps.EmployeeConfig',
+    'pacientVacines.apps.PacientvacinesConfig'
 ]
 
 MIDDLEWARE = [

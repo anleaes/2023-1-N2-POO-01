f430d83f301f80ab9de9df77dd77736de592b948
FerCarvalhoRP <fernandacarvalhorp@gmail.com>
2023-06-11 17:58:57 -0300

    alteracaomodels

--- apps/campaings/models.py
+++ apps/campaings/models.py
@@ -1,5 +1,13 @@
-from typing import Self
+from typing import Any, Self
 from django.db import models
 
 # Create your models here.
-class campaings (models.Model):
+class Campaings (models.Model):
+    def __init__(self, id, nome_campanha, comeca_em, termina_em, tipo):
+        self._id = None
+        self._nome_campanha = models.CharField(max_length=100)
+        self._comeca_em = models.DateField ()
+        self._termina_em = models.DateField ()
+        self._tipo = models.CharField(max_length=200)
+
+        

a1eb6710282132860c028da39e953c021589488a
Rafael Machado <rafaeldmachado2001@gmail.com>
2023-06-11 15:00:08 -0300

    Model para a classe 'localStorage' criada

--- apps/localStorage/models.py
+++ apps/localStorage/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class localStorage(models.Model) :
+    vaccine = models.ForeignKey(Vacina, on_delete=models.CASCADE)
+    quantity = models.IntegerField("Quantidade", verbose_name="Quantidade",  default=0)
+    created_at = models.DateTimeField(verbose_name="Criado em", auto_now_add=True)
+
+
+    class Meta: 
+        verbose_name = "Estoque local"
+        ordering = ["id"]
+
+
+    def __str__(self):
+        return f"{self.vaccine} - {self.quantity} - {self.created_at}"

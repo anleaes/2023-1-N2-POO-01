879e380a91cdea21a44126109ddae1e59e64d7a3
Rafael Machado <rafaeldmachado2001@gmail.com>
2023-06-11 13:52:37 -0300

    Classe 'localStorage' adicionada em 'INSTALLED_APPS' dentro do arquivo vacina_app/settings.py

--- vacina_app/settings.py
+++ vacina_app/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'vaccineScheduling.apps.VaccineschedulingConfig',
     'vaccineSupplier.apps.VaccinesupplierConfig',
     'vacinationLocal.apps.VacinationLocalConfig',
+    'localStorage.apps.LocalstorageConfig'
 ]
 
 MIDDLEWARE = [

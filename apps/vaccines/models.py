806315c994c60114552676244159958cc35d93c4
DanielaO85 <daniela.da.silva@gmail.com>
2023-06-11 13:56:24 -0300

    v1.32 Ajuste para CharField em Vacina

--- apps/vaccines/models.py
+++ apps/vaccines/models.py
@@ -3,14 +3,14 @@ from vaccineSupplier.models import FornecedorVacina
 from datetime import date
 
 class Vacina(models.Model):
-    id = models.StringField('id', max_length=20)
+    id = models.CharField('id', max_length=20)
     nome_vacina = models.CharField('nome_vacina', max_length=50)
     tipo_vacina = models.CharField('tipo_vacina', max_length=30)
-    doses_necessarias = models.StringField('doses_necessarias', max_length=10)
+    doses_necessarias = models.CharField('doses_necessarias', max_length=10)
     forma_armazenamento = models.CharField('forma_armazenamento', max_length=30)  
-    temperatura = models.StringField('temperatura', max_length=50) 
-    intervalo_entre_doses = models.StringField('intervalo_entre_doses', max_length=50)    
-    numero_lote = models.StringField('numero_lote', max_length=50)     
+    temperatura = models.CharField('temperatura', max_length=50) 
+    intervalo_entre_doses = models.CharField('intervalo_entre_doses', max_length=50)    
+    numero_lote = models.CharField('numero_lote', max_length=50)     
     fabricada_em = models.DateField ('fabricada_em', max_length=10)
     validade = models.DateField ('validade', max_length=10)   
     fornecedor = models.ForeignKey(FornecedorVacina, on_delete=models.CASCADE)     

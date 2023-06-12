886d9ff7e2d793adb727e4e40eb734c6683c7adc
Rafael Machado <rafaeldmachado2001@gmail.com>
2023-06-12 19:02:07 -0300

    Correção na classe 'Employee'

--- apps/employee/models.py
+++ apps/employee/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from vacinationLocal.models import VacinationLocal
 
 # Create your models here.
 class Employee(models.Model):
-    codigo = models.IntegerField()
-    nome_completo = models.CharField(max_length=100)
-    idade = models.IntegerField()
-    funcao = models.CharField(max_length=100)
-    formacao = models.CharField(max_length=100)
+    code = models.IntegerField("Código", default=0, null=False)
+    full_name = models.CharField("Nome completo", max_length=120, null=False, default='Incompleto')
+    age = models.IntegerField("Idade", default=0 ,null=False)
+    function = models.CharField("Função", max_length=150, null=False, default='Não informado')
+    formation = models.CharField("Formação", max_length=120, null=False, default='Não informado')
+    vacination_local = models.ForeignKey(VacinationLocal, on_delete=models.CASCADE, related_name="local_funcionario", default=0)
+
+    class Meta:
+        verbose_name = "Funcionário"
+        verbose_name_plural = "Funcionários"
+        ordering = ['id']
 
     def __str__(self):
-        return self.nome_completo
+        return f"{self.code} - {self.full_name} - {self.age} - {self.function} - {self.formation} - {self.vacination_local}"
